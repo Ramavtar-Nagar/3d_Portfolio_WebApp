f5e5dd3c08acb743928bacbc50634ac85396cfcd
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-08-01 23:45:40 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+        <div>
+            <h1 className="text-3xl font-bold underline text-red-500">
+                Hello world!
+            </h1>
+        </div>
+    </main>
+  )
+}
+
+export default App
